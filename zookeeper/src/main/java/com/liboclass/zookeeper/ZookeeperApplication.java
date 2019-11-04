2144fa9b98cb18ca39617b3cd05a753f8e6af9b6
libo <libo01@oursdfl.com>
2019-11-04 23:14:31 +0800

    修改过代码

--- zookeeper/src/main/java/com/liboclass/zookeeper/ZookeeperApplication.java
+++ zookeeper/src/main/java/com/liboclass/zookeeper/ZookeeperApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZookeeperApplication {
-
+    //this is a test
     private static final String CONNECT_ADDR = "192.168.183.100:2181,192.168.183.101:2181,192.168.183.102:2181";
 
     private static final int SESSION_OUTTIME= 5000;//ms
